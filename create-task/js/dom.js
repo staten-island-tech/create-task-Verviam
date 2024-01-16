7124f68fc46dda2abf490ae755573e3695810d20
Evan Gao <gaoevan37@gmail.com>
2024-01-16 09:24:27 -0500

    aaaaaaaaaaa

---
+++ create-task/js/dom.js
@@ -0,0 +1,7 @@
+const DOMSelectors = {
+    toDoList: document.querySelector(".toDos"),
+    addButton: document.getElementById("submit-button"),
+    userInput: document.getElementById("user-input")
+}
+
+export {DOMSelectors}
--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,51 +1,14 @@
-import "../css/style.css";
+import  {DOMSelectors} from "./dom.js"
 
-let findDiff = function (s, t) {
-  let sortS = [...s].sort();
-  let sortT = [...t].sort();
-  for (let i = 0; i < sortT.length; i++) {
-    if (sortT[i] != sortS[i]) {
-      return sortT[i];
-    }
-  }
-};
-console.log(findDiff("abdcef", "abcduef"));
+const ToDoItems = []
 
+function addToDo () {
+  const inputtedToDo = DOMSelectors.userInput.value
+  ToDoItems.push(inputtedToDo)
+  console.log(ToDoItems)
+  DOMSelectors.toDoList.insertAdjacentHTML("beforeend", 
+  `<div class = "to-do-card"></div>`)
+ 
+}
 
-
-let mergeFun = function (str1, str2) {
-  let merged = "";
-  let distance = Math.max(str1.length, str2.length);
-  for (let i = 0; i < distance; i++) {
-    if (i < str1.length) {
-      merged = merged + str1[i];
-    }
-    if (i < str2.length) {
-      merged = merged + str2[i];
-    }
-  }
-  return merged;
-};
-console.log(mergeFun("abc", "defgdfghdfghdfghfhdg"));
-
-
-
-let isMono = function (arr) {
-  let direction = 0;
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > arr[i - 1]) {
-      if (direction === 0) {
-        direction = 1;
-      } else if (direction === -1) {
-        return false;
-      }
-    } else if (arr[i] < arr[i - 1]) {
-      if (direction === 0) {
-        direction = -1;
-      } else if (direction === 1) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+DOMSelectors.addButton.addEventListener("click", addToDo)
