c0b19cc4cf69a7fdc5503a1808aacf49deb831f2
Evan Gao <gaoevan37@gmail.com>
2024-01-30 09:12:51 -0500

    aaaaaaaaaaaaaaaaaaaaa

--- create-task/js/dom.js
+++ create-task/js/dom.js
@@ -1,7 +1,8 @@
 const DOMSelectors = {
     toDoList: document.querySelector(".toDos"),
     submitButton: document.getElementById("submit-button"),
-    userInput: document.getElementById("user-input")
+    userInput: document.getElementById("user-input"),
+    removeButton: document.getElementById("remove-reminder")
 }
 
 export {DOMSelectors}
--- create-task/js/main.js
+++ create-task/js/main.js
@@ -2,22 +2,35 @@ import  {DOMSelectors} from "./dom.js"
 import "../css/style.css"
 
 const ToDoItems = []
+const updatedToDoItems = []
 
 function addToDo (event) {
   const inputtedToDo = DOMSelectors.userInput.value
   event.preventDefault()
   ToDoItems.push(inputtedToDo);
 
-    ToDoItems.forEach(inputs => {
-    DOMSelectors.toDoList.insertAdjacentHTML("beforeend", 
-    `<div class = "to-do-card">${inputs}</div>`)
-  });
+  for (i=0; i<ToDoItems.length; i++){
+    if (ToDoItems[i].includes(ToDoItems[i+1])){
+  // if it's already displayed, dont for each it
+  // if more than 1 to do, put on new line
+    }
+    else {
+      updatedToDoItems.push()
+      updatedToDoItems.forEach(inputs => {
+        DOMSelectors.toDoList.insertAdjacentHTML("beforeend", 
+        `<div class="card"><div class = "to-do-card">${inputs}</div>
+        <button type ="submit" class="remove-button" id="remove-reminder"> Remove </button>
+        </div>`)})
+    }
+  }  
+}
+
+function removeToDo () {
 
 }
 
- 
-// if it's already displayed, dont for each it
-// if more than 1 to do, put on new line
 // add remove button
 // check for requirements
+
 DOMSelectors.submitButton.addEventListener("click", addToDo)
+DOMSelectors.removeButton.addEventListener("click", removeToDo)
